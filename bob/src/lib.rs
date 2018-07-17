b9b99b067a81894f1e74df2e9842c29ae77be358
Brandon Bauley <bbauley@pdx.edu>
2018-07-17 11:45:29 -0700

    Got 7 tests to pass for the Bob assignment.

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,3 +1,59 @@
+/*
+Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+
+Bob answers 'Sure.' if you ask him a question.
+
+He answers 'Whoa, chill out!' if you yell at him.
+
+He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
+
+He says 'Fine. Be that way!' if you address him without actually saying
+anything.
+
+He answers 'Whatever.' to anything else.
+*/
+
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+
+    let mut yelling = true;
+    let mut question = true;
+    let remove_whitespace = message.split_whitespace(); //Remove any whitespaces
+    let clone = remove_whitespace.clone();
+    let num_of_words = remove_whitespace.count();   //TODO NEED TO CLEAN UP CODE
+    for (i, word) in clone.enumerate() {  //Loop through each word
+        let num_of_letters = word.len();
+        for (j, letter) in word.chars().enumerate() {   //Loop through each character in each word 
+
+            /* Checks for the yelling condition */
+            if yelling && letter.is_alphanumeric() {
+                if letter.is_lowercase() {
+                    println!("SET YELLING TO FALSE");
+                    yelling = false;
+                }
+            }
+
+            /*Check for question condition */
+            if question && j == num_of_words - 1 {  
+                if i == num_of_letters - 1 {        //Check the last word and letter for ?
+                    if letter != '?' {
+                        println!("SET QUESTION TO FALSE");
+                        question = false;
+                    }
+                }
+            }
+        }
+    }    
+
+    if yelling && question {
+        "Calm down, I know what I'm doing!"
+    }
+    else if yelling {
+        "Whoa, chill out!"
+    }
+    else if question {
+        "Sure."
+    }
+    else {
+        "Whatever."
+    }
 }
--- bob/tests/bob.rs
+++ bob/tests/bob.rs
@@ -6,19 +6,16 @@ fn test_stating_something() {
 }
 
 #[test]
-#[ignore]
 fn test_shouting() {
     assert_eq!("Whoa, chill out!", bob::reply("WATCH OUT!"));
 }
 
 #[test]
-#[ignore]
 fn test_shouting_gibberish() {
     assert_eq!("Whoa, chill out!", bob::reply("FCECDFCAAB"));
 }
 
 #[test]
-#[ignore]
 fn test_asking() {
     assert_eq!(
         "Sure.",
@@ -27,25 +24,21 @@ fn test_asking() {
 }
 
 #[test]
-#[ignore]
 fn test_ask_numeric_question() {
     assert_eq!("Sure.", bob::reply("You are, what, like 15?"));
 }
 
 #[test]
-#[ignore]
 fn test_asking_gibberish() {
     assert_eq!("Sure.", bob::reply("fffbbcbeab?"));
 }
 
 #[test]
-#[ignore]
 fn test_exclaiming() {
     assert_eq!("Whatever.", bob::reply("Let's go make out behind the gym!"));
 }
 
 #[test]
-#[ignore]
 fn test_using_acronyms_in_regular_speech() {
     assert_eq!(
         "Whatever.",
@@ -54,7 +47,6 @@ fn test_using_acronyms_in_regular_speech() {
 }
 
 #[test]
-#[ignore]
 fn test_forceful_question() {
     assert_eq!(
         "Calm down, I know what I'm doing!",
@@ -63,25 +55,21 @@ fn test_forceful_question() {
 }
 
 #[test]
-#[ignore]
 fn test_shouting_numbers() {
     assert_eq!("Whoa, chill out!", bob::reply("1, 2, 3 GO!"));
 }
 
 #[test]
-#[ignore]
 fn test_only_numbers() {
     assert_eq!("Whatever.", bob::reply("1, 2, 3"));
 }
 
 #[test]
-#[ignore]
 fn test_question_with_only_numbers() {
     assert_eq!("Sure.", bob::reply("4?"));
 }
 
 #[test]
-#[ignore]
 fn test_shouting_with_special_characters() {
     assert_eq!(
         "Whoa, chill out!",
@@ -90,25 +78,21 @@ fn test_shouting_with_special_characters() {
 }
 
 #[test]
-#[ignore]
 fn test_shouting_with_no_exclamation_mark() {
     assert_eq!("Whoa, chill out!", bob::reply("I HATE YOU"));
 }
 
 #[test]
-#[ignore]
 fn test_statement_containing_question_mark() {
     assert_eq!("Whatever.", bob::reply("Ending with ? means a question."));
 }
 
 #[test]
-#[ignore]
 fn test_non_letters_with_question() {
     assert_eq!("Sure.", bob::reply(":) ?"));
 }
 
 #[test]
-#[ignore]
 fn test_prattling_on() {
     assert_eq!(
         "Sure.",
@@ -135,7 +119,6 @@ fn test_alternate_silence() {
 }
 
 #[test]
-#[ignore]
 fn test_multiple_line_question() {
     assert_eq!(
         "Whatever.",
@@ -144,13 +127,11 @@ fn test_multiple_line_question() {
 }
 
 #[test]
-#[ignore]
 fn test_starting_with_whitespace() {
     assert_eq!("Whatever.", bob::reply("         hmmmmmmm..."));
 }
 
 #[test]
-#[ignore]
 fn test_ending_with_whitespace() {
     assert_eq!(
         "Sure.",
@@ -165,7 +146,6 @@ fn test_other_whitespace() {
 }
 
 #[test]
-#[ignore]
 fn test_non_question_ending_with_whitespace() {
     assert_eq!(
         "Whatever.",
