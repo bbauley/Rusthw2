d526f3fdd78e2aa21fcec9ea259c17d8000fed6d
Brandon Bauley <bbauley@pdx.edu>
2018-07-18 17:27:23 -0700

    Passed 19 tests for raindrops. Finished raindrops assignment.

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,24 @@
 pub fn raindrops(n: usize) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+
+  let mut result = "".to_string();
+  let mut factor = false;
+  if n % 3 == 0 {
+    factor = true;
+    result.push_str("Pling");
+  }
+  if n % 5 == 0 {
+    factor = true;
+    result.push_str("Plang");
+  }
+  if n % 7 == 0 {
+    factor = true;
+    result.push_str("Plong");
+  }
+
+  if factor {
+    result
+  }
+  else {
+    n.to_string()
+  }
 }
