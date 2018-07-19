b84a4143c1db83146ad5e3426d54dd3f99170df5
Brandon Bauley <bbauley@pdx.edu>
2018-07-19 16:48:41 -0700

    Passed 20 tests for robot-simulator. Finished robot-simulator

--- robot-simulator/src/lib.rs
+++ robot-simulator/src/lib.rs
@@ -9,37 +9,67 @@ pub enum Direction {
     West,
 }
 
-pub struct Robot;
+pub struct Robot {
+  coordinate: (isize, isize),
+  facing: Direction,
+}
 
 impl Robot {
     pub fn new(x: isize, y: isize, d: Direction) -> Self {
-        unimplemented!("Create a robot at (x, y) ({}, {}) facing {:?}", x, y, d,)
+        Robot {coordinate: (x, y), facing: d}
     }
 
     pub fn turn_right(self) -> Self {
-        unimplemented!()
+      let mut direction = self.facing;
+        match direction {
+          Direction::North =>  direction = Direction::East,
+          Direction::East  =>  direction = Direction::South,  
+          Direction::South =>  direction = Direction::West,
+          Direction::West  =>  direction = Direction::North,
+        }
+      Robot {coordinate: self.coordinate, facing: direction}
     }
 
     pub fn turn_left(self) -> Self {
-        unimplemented!()
+      let mut direction = self.facing;
+        match direction {
+          Direction::North =>  direction = Direction::West,
+          Direction::East  =>  direction = Direction::North,  
+          Direction::South =>  direction = Direction::East,
+          Direction::West  =>  direction = Direction::South,
+        }
+      Robot {coordinate: self.coordinate, facing: direction}
     }
 
     pub fn advance(self) -> Self {
-        unimplemented!()
+      let mut coordinate = self.coordinate; 
+        match self.facing { //The direction will tell me which axis to add/subtract from
+          Direction::North =>  coordinate.1 += 1,
+          Direction::East  =>  coordinate.0 += 1,  
+          Direction::South =>  coordinate.1 -= 1,
+          Direction::West  =>  coordinate.0 -= 1,
+        }
+      Robot {coordinate: coordinate, facing: self.facing}
     }
 
     pub fn instructions(self, instructions: &str) -> Self {
-        unimplemented!(
-            "Follow the given sequence of instructions: {}",
-            instructions
-        )
+      let mut robot = Robot{coordinate: self.coordinate, facing: self.facing};
+      for letter in instructions.chars() {
+        match letter {
+          'A' => robot = robot.advance(),
+          'L' => robot = robot.turn_left(),
+          'R' => robot = robot.turn_right(),
+           _  => ()
+        }
+      }
+      robot
     }
 
     pub fn position(&self) -> (isize, isize) {
-        unimplemented!()
+        self.coordinate
     }
 
     pub fn direction(&self) -> &Direction {
-        unimplemented!()
+        &self.facing
     }
 }
